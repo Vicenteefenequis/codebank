03d76b9098fe9bb12d2e247fc64cd83a67ad6f6f
Vicente Junior <vicente19981@live.com>
2023-03-25 14:05:44 -0300

    feat: create process transaction usecase

---
+++ usecase/process_transaction.go
@@ -0,0 +1,58 @@
+package usecase
+
+import (
+	"time"
+
+	"github.com/Vicenteefenequis/codebank/domain"
+	"github.com/Vicenteefenequis/codebank/dto"
+)
+
+type UseCaseTransaction struct {
+	TransactionRepository domain.TransactionRepository
+}
+
+func NewUseCaseTransaction(transactionRepository domain.TransactionRepository) UseCaseTransaction {
+	return UseCaseTransaction{
+		TransactionRepository: transactionRepository,
+	}
+}
+
+func (u UseCaseTransaction) ProcessTransaction(transactionDto dto.Transaction) (domain.Transaction, error) {
+	creditCard := u.hydrateTransaction(transactionDto)
+	ccBalanceAndLimit, err := u.TransactionRepository.GetCreditCard(*creditCard)
+	if err != nil {
+		return domain.Transaction{}, err
+	}
+	creditCard.ID = ccBalanceAndLimit.ID
+	creditCard.Limit = ccBalanceAndLimit.Limit
+	creditCard.Balance = ccBalanceAndLimit.Balance
+
+	t := u.newTransaction(transactionDto, ccBalanceAndLimit)
+	t.ProccessAndValidate(creditCard)
+
+	err = u.TransactionRepository.SaveTransaction(*t, *creditCard)
+	if err != nil {
+		return domain.Transaction{}, err
+	}
+	return *t, nil
+}
+
+func (u UseCaseTransaction) hydrateTransaction(transactionDto dto.Transaction) *domain.CreditCard {
+	creditCard := domain.NewCreditCard()
+	creditCard.Name = transactionDto.Name
+	creditCard.Number = transactionDto.Number
+	creditCard.ExpirationMonth = transactionDto.ExpirationMonth
+	creditCard.ExpirationYear = transactionDto.ExpirationYear
+	creditCard.CVV = transactionDto.CVV
+	return creditCard
+}
+
+func (u UseCaseTransaction) newTransaction(transactionDto dto.Transaction, cc domain.CreditCard) *domain.Transaction {
+	t := domain.NewTransaction()
+	t.CreditCardId = cc.ID
+	t.Amount = transactionDto.Amount
+	t.Store = transactionDto.Store
+	t.Description = transactionDto.Description
+	t.CreatedAt = time.Now()
+	return t
+}
